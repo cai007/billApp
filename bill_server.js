b96d5409099fd1237d081c86e90e8a4750bb735f
lanxuan007 <2427270009@qq.com>
2020-04-26 16:44:55 +0800

    添加家庭成员余额设置等接口，并修复后台崩溃bug

--- bill_server.js
+++ bill_server.js
@@ -56,6 +56,9 @@ function askUrl(server, db) {
                     case '/showUserBalance':
                         bill.showUserBalance(db, res, paras);
                         break;
+                    case '/showLastUsersBalance':
+                        bill.showLastUsersBalance(db, res, paras);
+                        break;
                     default:
                         badRequest(res);
                 }
@@ -77,6 +80,15 @@ function askUrl(server, db) {
                         break;
                     case '/deleteTypes':
                         bill.deleteTypes(db, req, res);
+                        break;
+                    case '/updateUserBalance':
+                        bill.updateUserBalance(db, req, res);
+                        break;
+                    case '/addUserBalance':
+                        bill.addUserBalance(db, req, res);
+                        break;
+                    case '/deleteUserBalance':
+                        bill.deleteUserBalance(db, req, res);
                         break;
                 }
                 break;
@@ -113,7 +125,7 @@ function dbConnect() {
             handleConnectError(err);
         }
         console.log('Server started...');
-        server.listen(3000, '127.0.0.1');
+        server.listen(3000, '192.168.101.37');
     });
 }
 
